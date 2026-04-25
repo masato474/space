16207fccb91852f3e241a61d9b39aa4248ab1748
masato474 <masato.iwbrd@gmail.com>
2026-04-25 14:14:33 +0900

    update

---
+++ data/data.js
@@ -0,0 +1,26 @@
+/*
+ * data.js
+ */
+
+const url = "data.json";	// 読み込むJSONファイル
+
+function formatJSON(json){
+
+	// JSONファイルを整形して表示
+	let html = "";
+	for(let i of json.data){
+		//console.log(i);
+		html += `<p>${i.id} ${i.res} / ${i.m}</p>`;
+	}
+	document.getElementById("result").innerHTML = html;
+}
+
+// 起動時の処理
+window.addEventListener("load", ()=>{
+
+	// 12星座情報を取得して表示
+	fetch(url)
+		.then( response => response.json())
+		.then( data => formatJSON(data));
+
+});
